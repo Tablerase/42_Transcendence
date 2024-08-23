917df542d09135fc33341bb4002b17616e4b62cf
rcutte <rcutte@student.42.fr>
2024-08-23 15:49:46 +0200

    feat: update todo webs security

--- srcs/requirements/django/app/backend/asgi.py
+++ srcs/requirements/django/app/backend/asgi.py
@@ -9,13 +9,18 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 from game.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
+"""
+TODO: Add AllowedHostsOriginValidator and AuthMiddleWareStack to the websocket_urlpatterns
+"""
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
--- srcs/requirements/django/app/game/consumers.py
+++ srcs/requirements/django/app/game/consumers.py
@@ -7,70 +7,70 @@ from channels.generic.websocket import WebsocketConsumer
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
-class GameConsumer(WebsocketConsumer):
-    game_group_name = "game_group"
-    players_positions = {}
+# class GameConsumer(WebsocketConsumer):
+#     game_group_name = "game_group"
+#     players_positions = {}
 
-    def connect(self):
-        self.player_id = str(uuid.uuid4())
-        self.accept()
+#     def connect(self):
+#         self.player_id = str(uuid.uuid4())
+#         self.accept()
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.game_group_name, self.channel_name
-        )
+#         async_to_sync(self.channel_layer.group_add)(
+#             self.game_group_name, self.channel_name
+#         )
 
-        # Initialize player position
-        self.players_positions[self.player_id] = {'x': 0, 'y': 0}
+#         # Initialize player position
+#         self.players_positions[self.player_id] = {'x': 0, 'y': 0}
 
-        # Send the player ID to the client
-        self.send(
-            text_data=json.dumps({
-                "type": "playerId",
-                "playerId": self.player_id
-            })
-        )
+#         # Send the player ID to the client
+#         self.send(
+#             text_data=json.dumps({
+#                 "type": "playerId",
+#                 "playerId": self.player_id
+#             })
+#         )
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.game_group_name, self.channel_name
-        )
+#     def disconnect(self, close_code):
+#         async_to_sync(self.channel_layer.group_discard)(
+#             self.game_group_name, self.channel_name
+#         )
 
-        # Remove player position
-        if self.player_id in self.players_positions:
-            del self.players_positions[self.player_id]
+#         # Remove player position
+#         if self.player_id in self.players_positions:
+#             del self.players_positions[self.player_id]
 
-    def receive(self, text_data=None, bytes_data=None):
-        if text_data:
-            try:
-                text_data_json = json.loads(text_data)
-                message_type = text_data_json.get('type')
+#     def receive(self, text_data=None, bytes_data=None):
+#         if text_data:
+#             try:
+#                 text_data_json = json.loads(text_data)
+#                 message_type = text_data_json.get('type')
 
-                if message_type == 'updatePosition':
-                    # Update player position
-                    self.players_positions[self.player_id] = {
-                        'x': text_data_json['x'],
-                        'y': text_data_json['y']
-                    }
+#                 if message_type == 'updatePosition':
+#                     # Update player position
+#                     self.players_positions[self.player_id] = {
+#                         'x': text_data_json['x'],
+#                         'y': text_data_json['y']
+#                     }
 
-                    # Broadcast updated positions to all clients
-                    async_to_sync(self.channel_layer.group_send)(
-                        self.game_group_name,
-                        {
-                            'type': 'broadcastPositions',
-                            'positions': self.players_positions
-                        }
-                    )
-                else:
-                    # Handle other message types
-                    pass
-            except json.JSONDecodeError:
-                self.send(text_data=json.dumps({'error': 'Invalid JSON received'}))
-        else:
-            self.send(text_data=json.dumps({'error': 'No data received'}))
+#                     # Broadcast updated positions to all clients
+#                     async_to_sync(self.channel_layer.group_send)(
+#                         self.game_group_name,
+#                         {
+#                             'type': 'broadcastPositions',
+#                             'positions': self.players_positions
+#                         }
+#                     )
+#                 else:
+#                     # Handle other message types
+#                     pass
+#             except json.JSONDecodeError:
+#                 self.send(text_data=json.dumps({'error': 'Invalid JSON received'}))
+#         else:
+#             self.send(text_data=json.dumps({'error': 'No data received'}))
 
-    def broadcastPositions(self, event):
-        positions = event['positions']
-        self.send(text_data=json.dumps({
-            'type': 'positions',
-            'positions': positions
-        }))
+#     def broadcastPositions(self, event):
+#         positions = event['positions']
+#         self.send(text_data=json.dumps({
+#             'type': 'positions',
+#             'positions': positions
+#         }))
