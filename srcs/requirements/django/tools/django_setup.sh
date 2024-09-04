e0a1d366363ffb936bf10441a44b68e307361fea
rcutte <rcutte@student.42.fr>
2024-09-04 17:29:07 +0200

    fix: script calculation error

--- srcs/requirements/django/tools/django_setup.sh
+++ srcs/requirements/django/tools/django_setup.sh
@@ -9,8 +9,8 @@ interval=1
 echo "Waiting for postgres..."
 while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
     sleep $interval
-    timeout=$($timeout - $interval)
-    if [ $timeout -eq 0 ]; then
+    timeout=$((timeout - interval))
+    if [ $timeout -le 0 ]; then
         echo "PostgreSQL failed to start"
         exit 1
     fi
