d78fc6bfc70266caeee90c568de3dc6b6f1df614
Simon Mac <MonkeyManDeveloper@protonmail.com>
2024-09-05 15:58:30 +0200

    profile-pic edit working

--- srcs/requirements/django/app/users/models/FriendRequest_model.py
+++ srcs/requirements/django/app/users/models/FriendRequest_model.py
@@ -1,11 +1,10 @@
 import django
 from django.db import models
-from django.conf import settings
-
+from users.models.User_model import CustomUser
 class FriendRequest(models.Model):
   from_user = models.ForeignKey(
-    settings.AUTH_USER_MODEL, related_name='from_user', on_delete=models.CASCADE
+    CustomUser, related_name='from_user', on_delete=models.CASCADE
   )
   to_user = models.ForeignKey(
-    settings.AUTH_USER_MODEL, related_name='to_user', on_delete=models.CASCADE
+    CustomUser, related_name='to_user', on_delete=models.CASCADE
   )
--- srcs/requirements/django/app/users/views/block_views.py
+++ srcs/requirements/django/app/users/views/block_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from users.models.User_model import CustomUser
+from django.db import transaction
 
 @login_required
 def block_user(request, userID):
@@ -8,13 +9,16 @@ def block_user(request, userID):
   user_to_block = get_object_or_404(CustomUser, id=userID)
 
   if user_to_block != current_user:
-    current_user.blocked_users.add(user_to_block)
-    current_user.friends.remove(user_to_block)
+    with transaction.atomic():
+      current_user.blocked_users.add(user_to_block)
+      if user_to_block in current_user.friends.all():
+        current_user.friends.remove(user_to_block)
+  
     
     request.session['modal_data'] = {
       'template': '_success_modal.html',
       'title': 'User Blocked',
-      'message': f'You have blocked <b><u>{user_to_block.username}</b></u> 🚫🛡️.</br>You will no longer see messages from this user.'
+      'message': f'You have blocked <b><u>{user_to_block.username}</b></u> 🚫🛡️.</br>'
     }
 
   return redirect(request.META.get('HTTP_REFERER', 'users'))
@@ -30,7 +34,7 @@ def unblock_user(request, userID):
     request.session['modal_data'] = {
       'template': '_success_modal.html',
       'title': 'User Unblocked',
-      'message': f'You have unblocked <b><u>{user_to_unblock.username}</b></u> 🔓.</br>You may now see messages from this user.'
+      'message': f'You have unblocked <b><u>{user_to_unblock.username}</b></u> 🔓.</br>'
     }
 
   return redirect(request.META.get('HTTP_REFERER', 'users'))
